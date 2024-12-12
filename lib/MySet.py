97077bc361dd50057a2a15aad7400452c342779a
BlessingOkora <moraablessing082@gmail.com>
2024-12-12 10:32:37 +0300

    builds set class methods

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,27 @@
 class MySet:
 
-    pass
+    def __init__(self, enumarable = []):
+        self.dictionary = {}
+        for value in enumarable:
+            self.dictionary[value] = True
+
+    def has(self, value):
+      return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+      self.dictionary.pop(value, None)
+      return self
+    
+    def size(self):
+      return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
+
+    def __str__(self):
+       dictionary_str = ','.join(map(str, sorted(self.dictionary)))
+       return f'MySet: {{{dictionary_str}}}'
